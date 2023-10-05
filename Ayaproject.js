d312e4403b6aea5e4af4109f26fc9b64e572fc0d
doaa2002a <rere1999ff@gmail.com>
2023-10-05 09:41:21 +0300

    first commit

---
+++ Ayaproject.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    // اجلب عنصر العنوان
+    var heading = document.querySelector('message');
+
+    // قم بتغيير نص العنوان
+    heading.textContent = "مرحبًا بك في موقعي الجديد";
+};
